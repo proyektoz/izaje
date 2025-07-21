a255cd5f91d2bcbf59ae3d7cd81676d808a3ccef
proyektoz <proyektoz2020@hotmail.com>
2025-07-21 10:48:55 -0500

    Page tierracoll

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import icon from 'astro-icon';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://proyektoz.github.io',
+  base: 'https://proyektoz.github.io/tierracoll/',
+  integrations: [icon()]
+});
