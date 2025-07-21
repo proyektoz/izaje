4ecc46366341b5983a1d1747cafef3def1f25c71
proyektoz <proyektoz2020@hotmail.com>
2025-07-21 11:07:09 -0500

    Fix file yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://proyektoz.github.io',
-  base: 'https://proyektoz.github.io/tierracoll/',
+  base: 'izaje',
   integrations: [icon()]
 });
